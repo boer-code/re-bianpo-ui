fa4f24ce8565fe3ef46d151df1cf9ae8abcbd97c
a64673 <99586658@qq.com>
2026-05-12 15:59:54 +0800

    feature:图表新增快捷选择"当天"选项 chore:更新tsconfig.json

--- apps/web-antd/src/components/shortcut-date-range-picker/shortcut-date-range-picker.vue
+++ apps/web-antd/src/components/shortcut-date-range-picker/shortcut-date-range-picker.vue
@@ -18,11 +18,12 @@ const times = ref<[Dayjs, Dayjs]>(); // 日期范围
 
 const rangePickerProps = getRangePickerDefaultProps();
 const timeRangeOptions = [
+  rangePickerProps.presets[0]!, // 当天
   rangePickerProps.presets[3]!, // 昨天
   rangePickerProps.presets[1]!, // 最近 7 天
   rangePickerProps.presets[2]!, // 最近 30 天
 ];
-const timeRangeType = ref(timeRangeOptions[1]!.label); // 默认选中第一个选项
+const timeRangeType = ref(timeRangeOptions[2]!.label); // 默认选中最近 7 天
 
 /** 设置时间范围 */
 function setTimes() {
